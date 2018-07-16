6a8c61cd27bd77bfdc64b6863394065fac34f7bc
Dmitriy Kamkin <solleks777@gmail.com>
2018-07-16 11:15:15 +0800

    ex1-19 Добавил группу главного по опросам, добавил пользователя, включил его в эту группу. Дал пользователю права на чтение папки bitrix и bitrix/admin, добавил права на запись в разделах 'Опросы' и 'Отзывы'

--- .top.menu.php
+++ .top.menu.php
@@ -62,6 +62,13 @@ $aMenuLinks = Array(
 		Array(), 
 		Array(), 
 		"" 
+	),
+	Array(
+		"Опросы", 
+		"/ask/", 
+		Array(), 
+		Array(), 
+		"" 
 	)
 );
 ?>
---
+++ ask/index.php
@@ -0,0 +1,6 @@
+<?
+require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
+$APPLICATION->SetTitle("Опросы");
+?>Здесь мы опрашиваем народ.<br>
+Посмотрите налево.<br>
+ <br><?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
